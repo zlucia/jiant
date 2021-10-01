0d20e1c8001976c78d324df917e3e84720a9b5d0
Lucia Zheng <zlucia@stanford.edu>
2021-10-01 11:05:59 -0700

    Fix CaseHOLD path argument for load_dataset

--- jiant/scripts/download_data/dl_datasets/hf_datasets_tasks.py
+++ jiant/scripts/download_data/dl_datasets/hf_datasets_tasks.py
@@ -188,7 +188,7 @@ HF_DATASETS_CONVERSION_DICT = {
         "phase_map": {"validation": "val", "challenge": "test"},
     },
     "casehold": {
-        "path": "casehold",
+        "path": "zlucia/casehold",
         "phase_list": ["train"],
         "label_map": {"0": 0, "1": 1, "2": 2, "3": 3, "4": 4},
     },
